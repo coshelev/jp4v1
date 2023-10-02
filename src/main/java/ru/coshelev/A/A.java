7c4ab1742fa829e7d46a4dbc4e0eb3e31d09d791
coshelev <coshelev@gmail.com>
2023-10-02 16:25:47 +0300

    5

--- src/main/java/ru/coshelev/A/A.java
+++ src/main/java/ru/coshelev/A/A.java
@@ -16,25 +16,27 @@ class A{
 	public static String AUTOBROKERMAIL_LOGIN    = "";
 	public static String AUTOBROKERMAIL_PASSWORD = "";
 
+    public static String LPARTSMAIL_HOST     = "";
+	public static String LPARTSMAIL_LOGIN    = "";
+	public static String LPARTSMAIL_PASSWORD = "";
+
 	public static void main(String[] args){
 		
 
-	LOG.info("{}; jvm {}",  "My example for info ", System.getProperty("java.version"));		
-	LOG.warn("This is my example for warning");
-	LOG.info("LOG.isDebugEnabled()= {}",LOG.isDebugEnabled());
-	LOG.debug("doStop {}", "This is exapmle for debug");
+	    LOG.info("{}; jvm {}",  "My example for info ", System.getProperty("java.version"));		
+	    LOG.warn("This is my example for warning");
+	    LOG.info("LOG.isDebugEnabled()= {}",LOG.isDebugEnabled());
+	    LOG.debug("doStop {}", "This is exapmle for debug");
 
-	if (LOG.isDebugEnabled())
+	    if (LOG.isDebugEnabled())
             LOG.debug("{}", "Debug enabled");
 
-	   Gson gson= new Gson();
-
-	   String buildinSettings = "{\"autobrokerMail.host\":\"imap.yandex.ru\", \"autobrokerMail.login\":\"luidorexpertALL\", \"autobrokerMail.password\":\"cfirdrfnabxyzmgk\",\"autobrokerMail.fileFound\":\"false\"}";
-
-                HashMap<String, String> map = gson.fromJson(buildinSettings, HashMap.class);
-		
+	    Gson gson= new Gson();
+	    String AUTOBROKERMAILSettings = "{\"autobrokerMail.host\":\"imap.yandex.ru\", \"autobrokerMail.login\":\"luidorexpertALL\", \"autobrokerMail.password\":\"cfirdrfnabxyzmgk\",\"autobrokerMail.fileFound\":\"false\"}";
+        HashMap<String, String> map = gson.fromJson(AUTOBROKERMAILSettings, HashMap.class);
+	
 		try{ 	
-			String filename = "myapp-1.1_settings1.json";
+			String filename = "AUTOBROKERMAIL.json";
 			File f = new File(filename);
                 	if (!f.exists())
                         	System.out.printf("settings file %1 does not exist \n", filename);
@@ -57,7 +59,36 @@ class A{
 		AUTOBROKERMAIL_LOGIN    = map.get("autobrokerMail.login");
 		AUTOBROKERMAIL_PASSWORD = map.get("autobrokerMail.password");
 		LOG.info(" AUTOBROKERMAIL: HOST = {}; LOGIN = {}; PASSWORD = {}", AUTOBROKERMAIL_HOST, AUTOBROKERMAIL_LOGIN, AUTOBROKERMAIL_PASSWORD);
-		
+
+        ///////////////////////////////////////////////////////////////
+
+        String LPARTSMAILSettings = "{\"autobrokerMail.host\":\"imap.yandex.ru\", \"autobrokerMail.login\":\"luidorexpertALL\", \"autobrokerMail.password\":\"cfirdrfnabxyzmgk\",\"autobrokerMail.fileFound\":\"false\"}";
+        HashMap<String, String> map = gson.fromJson(LPARTSMAILSettings, HashMap.class);
+	
+		try{ 	
+			String filename = "LPARTSMAIL.json";
+			File f = new File(filename);
+            if (!f.exists())
+                        	System.out.printf("settings file %1 does not exist \n", filename);
+			if  (f.exists()){
+				JsonReader reader = new JsonReader(new FileReader(filename));
+    				map = gson.fromJson(reader, HashMap.class);	
+				System.out.printf( "map to string: %s \n", gson.toJson(map));
+				};
+		}
+		catch (Exception e) {
+			System.out.print("exception!");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		};
+
+		System.out.printf("gson.toJson(map)=%s \n", gson.toJson(map));
+		System.out.printf("map.toString() = %s \n",map.toString());
+
+		LPARTSMAIL_HOST     = map.get("lpartsmail..host");
+		LPARTSMAIL_LOGIN    = map.get("lpartsmail.login");
+		LPARTSMAIL_PASSWORD = map.get("lpartsmail.password");
+
 		LOG.info("{}", " * class A before jetty *");
 		
 		//Start webserever
